ba582a5fff5b204d9111f49718f8e88db1e188ea
cccaaannn <can.kurt.aa@gmail.com>
2021-04-16 03:28:09 +0300

    more pages added, some innerHTMLs changed with createElement to prevent potential security issues

--- src/js/UI.js
+++ src/js/UI.js
@@ -28,19 +28,86 @@ class UI{
     renderMainPage(wordsArray){
         // renders main page
 
-        this.contentDiv.innerHTML = `<h2 class="wordHeader">Last added words</h2>`;
+        // <h2 class="wordHeader">All words</h2>
+        // <button class="lastAddedWordsButton" onClick="goToWord(this)" id="${wordsArray[i]}">-${wordsArray[i]}</button>
+        // <br>
+        // <button class="lastAddedWordsButton" onClick="goToWord(this)" id="${wordsArray[i]}">-${wordsArray[i]}</button>
+        // <br>
+        // <hr class="blue">
+
+    
+        this.contentDiv.innerHTML = ``;
+        const wordHeader = document.createElement("h2");
+        wordHeader.classList.add("wordHeader");
+        wordHeader.appendChild(document.createTextNode("Last added words"));
+        this.contentDiv.appendChild(wordHeader);
+
         for (let i = 0; i < wordsArray.length; i++) {
-            this.contentDiv.innerHTML += `
-            <button class="lastAddedWordsButton" onClick="goToWord(this)" id="${wordsArray[i]}">-${wordsArray[i]}</button>
-            <br>
-            `;
+
+            // ----- last added words buttons -----
+            const lastAddedWordsButton = document.createElement("button");
+            lastAddedWordsButton.classList.add("lastAddedWordsButton");
+            lastAddedWordsButton.id = wordsArray[i];
+            lastAddedWordsButton.setAttribute("onclick", "goToWord(this);");
+            lastAddedWordsButton.appendChild(document.createTextNode("-" + wordsArray[i]));
+
+            const br = document.createElement("br");
+
+            this.contentDiv.appendChild(lastAddedWordsButton);
+            this.contentDiv.appendChild(br);
+            // ----- ----- -----
         }
-        this.contentDiv.innerHTML += `<hr class="blue">`;
+
+        const Bluehr = document.createElement("hr");
+        Bluehr.classList.add("blue");
+        this.contentDiv.appendChild(Bluehr);
         
         // addNewWordForm
         this.renderAddWordForm();
     }
 
+    renderAllWords(allWords){
+        // renders all words page
+
+        // <h2 class="wordHeader">All words</h2>
+        // <button class="lastAddedWordsButton" onClick="goToWord(this)" id="${allWords[i]}">${i+1}-${allWords[i]}</button>
+        // <br>
+        // <button class="lastAddedWordsButton" onClick="goToWord(this)" id="${allWords[i]}">${i+1}-${allWords[i]}</button>
+        // <br>
+        // <hr class="blue">
+
+
+        this.contentDiv.innerHTML = ``;
+        const wordHeader = document.createElement("h2");
+        wordHeader.classList.add("wordHeader");
+        wordHeader.appendChild(document.createTextNode("All words"));
+        this.contentDiv.appendChild(wordHeader);
+
+        // this.contentDiv.innerHTML = ``
+        for (let i = 0; i < allWords.length; i++) {
+
+            // ----- all words words buttons -----
+            const lastAddedWordsButton = document.createElement("button");
+            lastAddedWordsButton.classList.add("lastAddedWordsButton");
+            lastAddedWordsButton.id = allWords[i];
+            lastAddedWordsButton.setAttribute("onclick", "goToWord(this);");
+            lastAddedWordsButton.appendChild(document.createTextNode(i+1 + "-" + allWords[i]));
+
+            const br = document.createElement("br");
+
+            this.contentDiv.appendChild(lastAddedWordsButton);
+            this.contentDiv.appendChild(br);
+            // ----- ----- -----
+
+        }
+
+        const Bluehr = document.createElement("hr");
+        Bluehr.classList.add("blue");
+        this.contentDiv.appendChild(Bluehr);
+
+        // addNewWordForm
+        this.renderAddWordForm();
+    }
 
     renderPosts(word, postsArray){
         // renders a post page
@@ -49,30 +116,99 @@ class UI{
         // post vote: postsArray[i][2]
         // post author: postsArray[i][3]
 
-        this.contentDiv.innerHTML = `<h2 class="wordHeader" id="wordHeader">${word}</h2>`
-    
+
+        // <div class="postDiv">
+        //     <h5>Author: ${postsArray[i][3]}</h5>
+        //     <p>${postsArray[i][1]}</p>
+        //     <div>
+        //         <button class="voteButton" onClick="vote(this)" id='{"word":"${word}", "postId":${postsArray[i][0]}, "type":true}'>⇧</button>
+        //         <button class="voteButton" onClick="vote(this)" id='{"word":"${word}", "postId":${postsArray[i][0]}, "type":false}'>⇩</button> ${postsArray[i][2]}
+        //     </div>
+        //     <hr class="blue">
+        // <div>
+        // 
+        // this.contentDiv.innerHTML += `
+
+        // <div class="postDiv">
+        //     <h5>Author: ${postsArray[i][3]}</h5>
+        // <div>
+
+
+        this.contentDiv.innerHTML = ``;
+
+        // h2 wordHeader author
+        const wordHeader = document.createElement("h2");
+        wordHeader.classList.add("wordHeader");
+        wordHeader.id = "wordHeader";
+        wordHeader.appendChild(document.createTextNode(word));
+
+        this.contentDiv.appendChild(wordHeader);
+
         for (let i = postsArray.length - 1; i >= 0; i--) {
-            this.contentDiv.innerHTML += `
-    
-            <div class="postDiv">
-                <h5>Author: ${postsArray[i][3]}</h5>
-                <p>${postsArray[i][1]}</p>
-                
-                <div>
-                    <button class="voteButton" onClick="vote(this)" id='{"word":"${word}", "postId":${postsArray[i][0]}, "type":true}'>⇧</button>
-                    <button class="voteButton" onClick="vote(this)" id='{"word":"${word}", "postId":${postsArray[i][0]}, "type":false}'>⇩</button> ${postsArray[i][2]}
-                </div>
-    
-                <hr class="blue">
-            <div>
-            `
+            // post div
+            const postDiv = document.createElement("div");
+
+            // author h5
+            const postAuthor = document.createElement("h5");
+            postAuthor.appendChild(document.createTextNode(`Author: ${postsArray[i][3]}`));
+
+            // post p
+            const post = document.createElement("p");
+            post.appendChild(document.createTextNode(postsArray[i][1]));
+
+            // hr
+            const Bluehr = document.createElement("hr");
+            Bluehr.classList.add("blue");
+
+
+            // ----- vote buttons -----
+            const voteDiv = document.createElement("div");
+
+            const upVoteButton = document.createElement("button");
+            const downVoteButton = document.createElement("button");
+
+            upVoteButton.classList.add("voteButton");
+            upVoteButton.id = `{"word":"${word}", "postId":${postsArray[i][0]}, "type":true}`;
+            upVoteButton.setAttribute("onclick", "vote(this);");
+            upVoteButton.appendChild(document.createTextNode("⇧"));
+
+            downVoteButton.classList.add("voteButton");
+            downVoteButton.id = `{"word":"${word}", "postId":${postsArray[i][0]}, "type":false}`;
+            downVoteButton.setAttribute("onclick", "vote(this);");
+            downVoteButton.addEventListener("click", vote);
+            downVoteButton.appendChild(document.createTextNode("⇩"));
+
+            const voteCount = document.createTextNode(postsArray[i][2]);
+            // ----- ----- -----
+
+            voteDiv.appendChild(upVoteButton);
+            voteDiv.appendChild(downVoteButton);
+            voteDiv.appendChild(voteCount);
+
+            postDiv.appendChild(postAuthor);
+            postDiv.appendChild(post);
+            postDiv.appendChild(voteDiv);
+            postDiv.appendChild(Bluehr);
+
+            this.contentDiv.appendChild(postDiv);
         }
 
         // addNewPostForm
         this.renderAddPostForm();
     }
 
+    renderAbout(){
+        const contentDiv = document.getElementById("contentDiv");
 
+        contentDiv.innerHTML = `
+        <div class="aboutDiv">
+            <h2 class="wordHeader">About</h2> 
+            <p>Distributed Dictionary is a user-created dictionary that runs entirely on the blockchain without the need for a centralized backend.</p>
+            <p>Adding or voting posts requires payment but posts are permanently saved to the blockchain. Pages can load slower depending on the current blockchain response time.</p>
+            <p>To use Distributed Dictionary you need web3 support on your browser, you can use an extension like <a target="_blank" href="https://metamask.io/">metamask</a>.</p>
+        </div>
+        `;
+    }
     /* ---------- ---------- ---------- */
 
 
@@ -161,13 +297,18 @@ class UI{
 
         // if isPermanent is true add a close button instead of delay
         if(isPermanent){
-            const closeButton = document.createElement("div");
-            closeButton.className = "toastMessageButton";
+            // button div
+            const closeButtonDiv = document.createElement("div");
+            closeButtonDiv.classList.add("toastMessageButton");
 
-            closeButton.innerHTML += `
-                <button class="toastMessageButton" onclick="return this.parentNode.parentNode.remove();">X</button>
-            `
-            alert.appendChild(closeButton);
+            // button
+            const closeButton = document.createElement("button");
+            closeButton.classList.add("toastMessageButton");
+            closeButton.setAttribute("onclick", "return this.parentNode.parentNode.remove();");
+            closeButton.appendChild(document.createTextNode("X"));
+
+            closeButtonDiv.appendChild(closeButton);
+            alert.appendChild(closeButtonDiv);
 
         }
         // delete massage after delay
